849cdbb372f91f9745895623325708f7a9d0f651
ALEX MOLL <alexmoll@ALEXs-MacBook-Pro.local>
2017-07-03 15:49:29 -0600

    add exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg {
+	public static void main( String[] args ) {
+		System.out.println( "Alex is determined to learn how to code." );
+		System.out.println( "Today's date is July 3, 2017" );
+	}
+}
